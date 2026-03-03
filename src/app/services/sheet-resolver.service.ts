adbaa52622edef4a4fb2a507271c2972b0c573b7
Kevin Triana <jeke20@gmail.com>
2026-03-03 12:40:41 -0500

    Fix sheet resolver

--- src/app/services/sheet-resolver.service.ts
+++ src/app/services/sheet-resolver.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { DataService } from './data.service';
+import { LocalStorageService } from './local-storage.service';
 import { ScheduledSong } from '../models/song.model';
+import { GROUP_LINKS } from '../constants/groups';
 
 @Injectable({ providedIn: 'root' })
 export class SheetResolver implements Resolve<ScheduledSong[]> {
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private localStorageService: LocalStorageService,
+    private router: Router
+  ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<ScheduledSong[]> {
-    const sheetParam = route.data && route.data['sheet'] ? route.data['sheet'] as string : undefined;
+    const group = this.localStorageService.get('grupo_alabanza');
+    if (!group || !GROUP_LINKS[group]) {
+      this.router.navigate(['/elegir-grupo']);
+      return of([]);
+    }
+
+    this.dataService.setSpreadsheet(GROUP_LINKS[group].link, GROUP_LINKS[group].sheet);
+
+    const sheetParam = route.data?.['sheet'] as string | undefined;
     return this.dataService.getPublicSheetData(sheetParam).pipe(
       catchError(err => {
         console.error('SheetResolver: error loading sheet', err);
